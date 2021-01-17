ff0fc6f67e68e9836d1486e04f886a701718bf02
Gogulaanand <gogulaanand02@gmail.com>
2021-01-17 19:48:25 +0530

    backend init files

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a92581cce41ba658b093e805e9f60057'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '523d4c6d-cd71-4fe9-baef-a1fa994b40ab'
+};
