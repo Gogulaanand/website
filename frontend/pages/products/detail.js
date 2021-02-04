a26220134fc4d16f0b6b65814493f2db4a4c89a9
Gogulaanand <gogulaanand02@gmail.com>
2021-02-04 09:33:56 +0530

    fix product page redirection, express server, dynamic routing init

--- frontend/pages/products/detail.js
+++
@@ -1,9 +0,0 @@
-import ProductDetail from "../../components/product/detail";
-
-export default function Detailpage(props) {
-  return (
-    <>
-      <ProductDetail props={props} />
-    </>
-  );
-}
--- frontend/pages/products/index.js
+++ frontend/pages/products/index.js
@@ -43,8 +43,7 @@ export default function Products() {
         <div className="mx-auto mt-36 grid grid-cols-3 grid-space-4 w-4/5">
           {data.products.map((res) => (
             <Card
-              className="lg:w-80 md:w-64 sm:w-44 mb-32 ml-16"
-              data-aos="animate-fadeInUp"
+              className="lg:w-80 md:w-64 sm:w-44 mb-32 ml-16 shadow-md animate-fadeInUp"
               key={res.id}
             >
               <Card.Img
@@ -55,8 +54,18 @@ export default function Products() {
               <Card.Body>
                 <Card.Title>{res.name}</Card.Title>
                 <Card.Text>{res.description}</Card.Text>
-                <Link href={`/products?id=${res.id}`} passHref>
-                  <Button className="mt-6">View</Button>
+                <Link
+                  href={`/products?id=${res.id}`}
+                  as={`/products/${res.name.replace(/\s/g, "-")}`}
+                  passHref
+                >
+                  <Button
+                    className="mt-6 cursor-pointer"
+                    variant="primary"
+                    as="a"
+                  >
+                    View
+                  </Button>
                 </Link>
               </Card.Body>
             </Card>
