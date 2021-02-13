f4c3f548450f3a9a65277bde4c0a2b21685e7338
Gogulaanand <gogulaanand02@gmail.com>
2021-02-13 22:27:17 +0530

    delete procfile

--- frontend/components/contact.js
+++ frontend/components/contact.js
@@ -71,10 +71,10 @@ export default function contactForm() {
             <div className="md:grid md:grid-cols-3 md:gap-6">
               <div className="md:col-span-1 grid items-center bg-indigo-600 rounded-md">
                 <div className="px-4 sm:px-0">
-                  <h1 className="text-md leading-6 text-white text-center">
+                  <h1 className="text-2xl leading-6 text-white text-center">
                     Contact Information
                   </h1>
-                  <p className="mt-3 text-sm text-white text-center mx-4">
+                  <p className="mt-3 text-md text-white text-center mx-4">
                     Fill up the form and we will get back to you within 24
                     hours.
                   </p>
@@ -87,9 +87,9 @@ export default function contactForm() {
                     >
                       <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                     </svg>
-                    <h3 className="text-md font-small text-white text-center mx-auto col-start-3 col-end-5">
+                    <p className="text-lg font-small text-white text-center mx-auto col-start-3 col-end-5">
                       +123 456 789
-                    </h3>
+                    </p>
                   </div>
                   <div className="grid grid-cols-5 grid-gap-2 mt-9">
                     <svg
@@ -101,9 +101,9 @@ export default function contactForm() {
                       <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                       <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                     </svg>
-                    <h3 className="text-md font-small text-white text-center mx-auto col-start-3 col-end-5">
+                    <p className="text-lg font-small text-white text-center mx-auto col-start-3 col-end-5">
                       abc@gmail.com
-                    </h3>
+                    </p>
                   </div>
                 </div>
               </div>
