11d3cfc62d71e2924520e8c66d07ab05f0ba99ab
Gogulaanand <gogulaanand02@gmail.com>
2021-02-22 09:07:26 +0530

    dynamic import svg components

--- frontend/components/register.js
+++ frontend/components/register.js
@@ -2,8 +2,13 @@ import { useState, useContext } from "react";
 import { registerUser } from "../lib/auth";
 import { useFormik } from "formik";
 import AppContext from "../context/AppContext";
-import SvgArrowPointingToRight from "./svg/SvgArrowPointingToRight";
 import { toaster } from "evergreen-ui";
+import dynamic from "next/dynamic";
+
+const SvgArrowPointingToRight = dynamic(() =>
+  import("./svg/SvgArrowPointingToRight")
+);
+const SvgLoading = dynamic(() => import("./svg/SvgLoading"));
 
 export default function Register() {
   const [loading, setLoading] = useState(false);
@@ -103,26 +108,7 @@ export default function Register() {
           Signup
         </button>
         {loading ? (
-          <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
+          <SvgLoading />
         ) : (
           <SvgArrowPointingToRight className="w-5 h-6 fill-current stroke-current text-white absolute inset-y-0 right-0 my-2 mr-3" />
         )}
