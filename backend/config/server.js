a317250436da81a1f7095c69ebdb6497bba0c406
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-03 17:29:30 +0530

    Update Strapi database settings (#21)
    * update db
    
    * authenticationDatabase
    
    * uri env variable
    
    * Update database.json
    
    * Update database.json
    
    * add graphql plugin, update database env
    
    * Update package.json
    
    * Update package.json
    
    * development env config
    
    * Update database.json
    
    * Update database.json
    
    * Update database.json
    
    * Revert "Update database.json"
    
    This reverts commit 2e394eefff6f6be87f8b4700c52bd6f63bae3250.
    
    * Update package.json
    
    * revamp v2
    
    * update database config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET", "a92581cce41ba658b093e805e9f60057"),
+      secret: env('ADMIN_JWT_SECRET', 'c2dd3cf65c31e3ba6583aaed3857ec4c'),
     },
   },
 });
--- backend/extensions/users-permissions/config/jwt.js
+++ backend/extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '523d4c6d-cd71-4fe9-baef-a1fa994b40ab'
+  jwtSecret: process.env.JWT_SECRET || '8abf1614-8c33-465a-839c-d6f6f20f579d'
 };
