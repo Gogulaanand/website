7df670f758196425b6dbb594313773f56e127681
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-05 09:28:35 +0530

    hidden empty columns for smaller screens

--- frontend/components/cart/filledCart.js
+++ frontend/components/cart/filledCart.js
@@ -9,13 +9,11 @@ export default function FilledCart() {
   const { cart } = appContext;
 
   return (
-    <div className="grid grid-cols-6 w-4/5 min-h-screen lg:mt-24 lg:mx-0 mx-auto">
-      <div className="lg:col-span-1"></div>
+    <div className="grid grid-cols-6 w-4/5 min-h-screen lg:mt-24 mt-12 lg:mx-0 mx-auto">
+      <div className="lg:col-span-1 hidden lg:block"></div>
       <div className="lg:col-span-4 col-span-6">
         <div className="flex flex-col overflow-auto">
-          <h1 className="md:text-2xl sm:text-xl font-semibold lg:ml-2">
-            Shopping cart
-          </h1>
+          <h1 className="font-semibold text-2xl">Shopping cart</h1>
           {cart.items &&
             cart.items.map((item) => {
               return <CartItem data={item} key={item.id} />;
@@ -35,7 +33,7 @@ export default function FilledCart() {
           </div>
         </div>
       </div>
-      <div className="lg:col-span-1"></div>
+      <div className="lg:col-span-1 hidden lg:block"></div>
     </div>
   );
 }
