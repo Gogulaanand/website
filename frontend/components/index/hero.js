09de79cc15c3b5e1c12b974eb5159a0e4d6b8137
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-15 09:09:32 +0530

    SEO optomization (#27)
    * antd min css, crawlable links
    
    * Update globals.sass
    
    * minor fixes

--- frontend/components/index/hero.js
+++ frontend/components/index/hero.js
@@ -23,7 +23,7 @@ export default function Hero() {
           <div className="flex items-center">
             <Link href="/products" passHref>
               <a className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded-full shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none cursor-pointer">
-                Explore
+                Explore Products
               </a>
             </Link>
             <Link href="/#contact" passHref>
