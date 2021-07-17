f87317c03981a7d1bb390d9f105cb5dd995a8781
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-17 21:36:34 +0530

    revamp hero section

--- frontend/components/index/hero.js
+++ frontend/components/index/hero.js
@@ -1,56 +1,56 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 export default function Hero() {
   return (
-    <div className="relative px-4 pt-16 mx-auto lg:py-32 md:px-8 xl:px-10 sm:max-w-xl md:max-w-full lg:h-screen">
-      <div className="max-w-xl mx-auto lg:max-w-screen-xl">
-        <div className="mb-16 lg:max-w-lg lg:mb-0 lg:mt-32">
-          <div className="max-w-xl mb-6">
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+    <div className="flex flex-col flex-1 h-full">
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative pt-6 container px-4 sm:px-6 lg:px-8 mx-auto">
+          <div className="mt-10 mx-auto max-w-4xl sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28 sm:text-center">
+            <h2 className="text-5xl tracking-tight leading-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-none md:text-5xl lg:text-6xl xl:text-7xl">
               The quick, brown fox
               <br className="hidden md:block" />
               jumps over{" "}
-              <span className="inline-block text-transparent bg-clip-text bg-gradient-r from-gradientBlue via-gradientPink to-gradientRed">
+              <span className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-red-400 py-1">
                 a lazy dog
               </span>
             </h2>
-            <p className="text-base text-gray-700 md:text-lg">
+            <p className="mt-12 text-xl sm:text-2xl leading-tight font-medium text-gray-500 max-w-4xl mx-auto ">
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
               quae. explicabo.
             </p>
-          </div>
-          <div className="flex items-center">
-            <Link href="/products" passHref>
-              <a className="inline-flex items-center justify-center h-12 px-8 py-3 border border-transparent text-base leading-6 font-semibold rounded-full text-white shadow-md bg-deep-purple-accent-400 cursor-pointer hover:text-white transition duration-150 ease-in-out md:py-3 md:text-lg md:px-10">
-                Explore Products
-                <svg
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  height="1em"
-                  width="1em"
-                  className="-mr-1 ml-3"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+            <div className="pt-6 pb-24 sm:text-center mx-auto md:mt-8">
+              <Link href="/products" passHref>
+                <a className="inline-flex items-center justify-center h-12 px-8 py-3 border border-transparent text-base leading-6 font-semibold rounded-full text-white shadow-md bg-gray-800 hover:hoverbg-gray-900 focus:outline-none focus:border-blue-700 cursor-pointer hover:text-white transition duration-150 ease-in-out md:py-3 md:text-lg md:px-10">
+                  Explore Products
+                  <svg
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    height="1em"
+                    width="1em"
+                    className="-mr-1 ml-3"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                  </svg>
+                </a>
+              </Link>
+              <Link href="/#contact" passHref>
+                <a
+                  aria-label=""
+                  className="inline-flex items-center justify-center text-base leading-6 font-semibold transition duration-150 ease-in-out md:py-3 md:text-lg px-3 md:px-10 text-gray-800 cursor-pointer hover:text-gray-800"
                 >
-                  <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-                </svg>
-              </a>
-            </Link>
-            <Link href="/#contact" passHref>
-              <a
-                aria-label=""
-                className="inline-flex items-center text-base leading-6 font-semibold transition duration-150 ease-in-out md:py-3 md:text-lg md:px-10 text-deep-purple-accent-400 cursor-pointer hover:text-deep-purple-accent-400"
-              >
-                Contact Us
-              </a>
-            </Link>
+                  Contact Us
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-center lg:mb-32">
+      {/* <div className="flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-center lg:mb-32">
         <Image
           src="/full-browser.jpeg"
           className="object-cover object-top w-4/5 h-64 max-w-xl -mb-16 rounded shadow-2xl lg:ml-32 xl:ml-4 lg:-mb-24 xl:-mb-28 lg:h-auto lg:max-w-screen-md"
@@ -58,7 +58,7 @@ export default function Hero() {
           width={700}
           height={550}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
