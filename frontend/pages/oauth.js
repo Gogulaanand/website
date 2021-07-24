a0730398dfc82027e0ea8f5becb6aa6e2b50c160
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-24 10:25:25 +0530

    check for user in oauth callback page

--- frontend/pages/oauth.js
+++ frontend/pages/oauth.js
@@ -1,27 +1,31 @@
 import { useRouter } from "next/router";
 import { useContext, useState, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 export default function OauthSuccess() {
   const router = useRouter();
   const [loginStatus, setLoginStatus] = useState(false);
-  const { checkUserLoggedIn } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
   useEffect(() => {
-    const token = checkUserLoggedIn();
-    if (token) {
+    if (user) {
       setLoginStatus(true);
-      setTimeout(() => router.push("/"), 2000);
+      setTimeout(() => router.push("/"), 1000);
     }
-  }, []);
+  }, [user]);
 
   return (
     <>
       <div className="h-screen w-screen flex">
         <div className="mx-auto lg:mt-48 md:mt-32 sm:mt-24 h-3/5 lg:w-1/5 md:w-2/5 sm:w-3/5">
           <div className="mb-12 text-center">
-            {loginStatus && <h1>Login Successful. Please wait...</h1>}
-            {!loginStatus && <h1>your are not logged in</h1>}
+            {loginStatus && (
+              <h1>Login Successful. Redirecting to homepage...</h1>
+            )}
+            {!loginStatus && <Spin indicator={antIcon} />}
           </div>
         </div>
       </div>
