73798d6bbd7d992b6d469fdb936fca0f0f824a47
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-26 22:38:59 +0530

    move login heading down for mobile & adjust width

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -5,9 +5,11 @@ export default function User() {
   return (
     <>
       <div className="h-screen w-screen flex">
-        <div className="mx-auto lg:mt-48 md:mt-32 sm:mt-24 h-3/5 lg:w-1/5 md:w-2/5 sm:w-3/5">
+        <div className="mx-auto lg:mt-48 md:mt-32 sm:mt-24 h-3/5 xl:w-3/12 lg:w-3/12 md:w-5/12 sm:w-3/5">
           <div className="mb-12 text-center">
-            <h1 className="text-2xl mb-4">Login / Create account</h1>
+            <h1 className="text-2xl mb-4 mt-32 md:mt-2">
+              Login / Create account
+            </h1>
           </div>
           <Login />
         </div>
