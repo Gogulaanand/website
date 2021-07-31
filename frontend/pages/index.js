2551b680038ca116e1ca4247f1fff2d052e9120d
Gogulaanand R <gogulaanand02@gmail.com>
2021-07-31 17:02:45 +0530

    add meta tags, minor account and success pages UI changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-
+import Head from "next/head";
 import Hero from "../components/index/hero";
 const Gallery = dynamic(() => import("../components/index/gallery"));
 const Contact = dynamic(() => import("../components/index/contact"));
@@ -7,6 +7,13 @@ const Content = dynamic(() => import("../components/index/content"));
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Sunfabb</title>
+        <meta
+          type="description"
+          content="Your one stop solution to order all your fabric requirements like bedspreads, pillow covers, bedsheets, towels and napkins"
+        />
+      </Head>
       <Hero />
       <Content />
       <Gallery />
