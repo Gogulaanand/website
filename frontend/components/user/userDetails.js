6e302a5d3782a3ea19b193ed24ed9b53e14b7e8f
Gogulaanand R <gogulaanand02@gmail.com>
2021-08-25 08:53:32 +0530

    antd menu font, increase font size in account

--- frontend/components/user/userDetails.js
+++ frontend/components/user/userDetails.js
@@ -7,7 +7,7 @@ export default function UserDetails() {
   const { user, logoutUser } = useContext(AuthContext);
   return (
     <div>
-      <p className="my-2 mx-4">Logged in as: {user}</p>
+      <p className="my-2 mx-4 text-lg">Logged in as: {user}</p>
       <Button href="#" onClick={logoutUser} className="my-2 mx-4">
         Logout
       </Button>
